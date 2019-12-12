7f92ff1e9bfb149d7742bf95d0cb5847f48b7357
khoi01 <khoi00091@gmail.com>
2019-12-12 13:09:25 +0800

    Basic Recycler View

--- app/src/main/java/com/khoi/kotlinreyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/khoi/kotlinreyclerviewexample/MainActivity.kt
@@ -2,11 +2,41 @@ package com.khoi.kotlinreyclerviewexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var blogAdapter:BlogRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initRecyclerView()
+        addDataSet()
+    }
+
+    private fun addDataSet(){
+        val data = DataSource.createDataSet();
+
+        //add data
+        blogAdapter.submitList(data)
+    }
+
+
+    private fun initRecyclerView(){
+
+        recycler_view.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+
+            //add top spacing padding
+            val topSpacingDecoration = TopSpacingItemDecoration(30)
+            addItemDecoration(topSpacingDecoration)
+
+            blogAdapter = BlogRecyclerAdapter()
+            adapter = blogAdapter
+        }
     }
 }
---
+++ app/src/main/java/com/khoi/kotlinreyclerviewexample/models/BlogPost.kt
@@ -0,0 +1,7 @@
+package com.khoi.kotlinreyclerviewexample.models
+
+//data class
+
+data class BlogPost(var title:String,var body:String,var image:String,var username:String){
+
+}
